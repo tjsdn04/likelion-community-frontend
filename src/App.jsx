8d27625a59076c0cdf9beba4577fa9ed4f7a9d13
nicerjs23 <nicerjs23@gmail.com>
2024-10-29 00:35:43 +0900

    [Fix] #11 - 스크롤 다시한번 수정

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,23 @@ import GlobalStyle from "@styles/global";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    const setVh = () => {
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    };
+
+    setVh(); // 초기 설정
+    window.addEventListener("resize", setVh); // 화면 크기 변경 시 업데이트
+
+    return () => {
+      window.removeEventListener("resize", setVh);
+    };
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
