b42aec778f098340349310a2fa57403c2f7e935d
tjsdn04 <sunwoo-0104@naver.com>
2024-11-10 03:32:03 +0900

    Feat: 회원가입 페이지 구현

--- src/router.jsx
+++ src/router.jsx
@@ -1,13 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import { DefaultLayout } from "@layout/DefaultLayout";
 import { MainPage } from "@pages/mainPage/MainPage";
-import { WelcomePage } from "@pages/loginPage/WelcomePage";
+import { WelcomePage } from "@pages/accouontPage/WelcomePage";
 import { SchMainPage } from "@pages/schMainPage/SchMainPage";
 import { AdminAttPage } from "@pages/adminAttPage/AdminAttPage";
 import { AdminAttRegisterPage } from "@pages/adminAttPage/AdminAttRegisterPage";
 import { AdminAttManagePage } from "@pages/adminAttPage/AdminAttManagePage";
 import { LionAttNum } from "@pages/lionAttPage/LionAttNum";
-import { LoginPage } from "@pages/loginPage/LoginPage";
+import { LoginPage } from "@pages/accountPage/LoginPage";
+import { SignUpPage } from "@pages/accountPage/SignUpPage";
+import { VerificationPage } from "@pages/accountPage/VerificationPage";
 import { ChatListPage } from "@pages/ChatPage/ChatListPage";
 import { ChatPage } from "@pages/ChatPage/ChatPage";
 
@@ -24,6 +26,9 @@ export const router = createBrowserRouter([
       { path: "adminAttManage", element: <AdminAttManagePage /> },
       { path: "lionAttNum", element: <LionAttNum /> },
       { path: "login", element: <LoginPage /> },
+      { path: "signup", element: <SignUpPage /> },
+      { path: "verification", element: <VerificationPage /> },
+      { path: "loding", element: <Loding /> },
       { path: "chatList", element: <ChatListPage /> },
       { path: "chat", element: <ChatPage /> },
     ],
