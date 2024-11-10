4f23bcd65ddd309bab5134866b15017af166edd4
nicerjs23 <nicerjs23@gmail.com>
2024-11-10 16:54:40 +0900

    [Feature] #53 - 로컬호스트https로 변경

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 // https://vitejs.dev/config/
+import fs from "fs";
+import dotenv from "dotenv";
+
+// .env 파일 로드
+dotenv.config();
+
 export default defineConfig({
+  server: {
+    https: {
+      key: fs.readFileSync(process.env.PEM_KEY_PATH),
+      cert: fs.readFileSync(process.env.PEM_CERT_PATH),
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
@@ -15,6 +27,7 @@ export default defineConfig({
       "@pages": path.resolve(__dirname, "src/pages"),
       "@styles": path.resolve(__dirname, "src/styles"),
       "@assets": path.resolve(__dirname, "src/assets"),
+      "@apis": path.resolve(__dirname, "src/apis"),
     },
   },
 });
