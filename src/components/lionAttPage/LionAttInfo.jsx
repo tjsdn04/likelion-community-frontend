aa7b756e1a96f4454cb44d64b661d01cc302a7fe
nicerjs23 <nicerjs23@gmail.com>
2024-11-15 01:22:15 +0900

    Fix: 아기사자 출석페이지 디자인수정중

---
+++ src/components/lionAttPage/LionAttInfo.jsx
@@ -0,0 +1,91 @@
+// 운영진 출석 정보 확인카드 컴포넌트
+import * as S from "@components/adminAttManage/AdminAttInfo.styled";
+import { useState } from "react";
+import list from "@assets/icons/list.svg";
+import dateIcon from "@assets/icons/date.svg";
+import location from "@assets/icons/location.svg";
+import image from "@assets/icons/image.svg";
+import EditDelModal from "@components/adminAttManage/EditDelModal";
+export const LionAttInfo = ({
+  date,
+  time,
+  place,
+  track,
+  title,
+  description,
+  file,
+}) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalMessage, setModalMessage] = useState("");
+
+  // 파일 이름 추출 함수
+  const getFileName = (fileUrl) => {
+    return fileUrl ? fileUrl.split("/").pop() : "파일이 없습니다.";
+  };
+
+  const openModal = (message) => {
+    setModalMessage(message);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleEdit = () => {
+    openModal("수정하시겠습니까?");
+  };
+
+  const handleDelete = () => {
+    openModal("삭제하시겠습니까?");
+  };
+  return (
+    <S.Wrapper>
+      <S.Top>
+        <S.TopImg src={list} />
+      </S.Top>
+      <S.Mid>
+        <S.Gap5>
+          <S.Date>
+            <div>
+              <S.DateImg src={dateIcon} />
+              <span>
+                {date} {time}~
+              </span>
+            </div>
+          </S.Date>
+          <S.Location>
+            <S.LocationImg src={location} />
+            {place}
+          </S.Location>
+          <S.Title>
+            [{track}] {title}
+          </S.Title>
+        </S.Gap5>
+        <S.Detail>{description}</S.Detail>
+
+        {file && (
+          <S.FileDiv>
+            <a href={file} target="_blank" rel="noopener noreferrer">
+              <S.FileName>{getFileName(file)}</S.FileName>
+            </a>
+            <img src={image} alt="file icon" />
+          </S.FileDiv>
+        )}
+      </S.Mid>
+      {isModalOpen && (
+        <EditDelModal
+          message={modalMessage}
+          onConfirm={() => {
+            // 확인 버튼 클릭 시 로직 추가
+            console.log(`${modalMessage} 확인`);
+            closeModal();
+          }}
+          onCancel={closeModal}
+        />
+      )}
+    </S.Wrapper>
+  );
+};
+
+export default LionAttInfo;
